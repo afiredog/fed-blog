07dcf877d1ea76cc166e2e3f99610a395c375a88
卡比 <adbluejoker@outlook.com>
2024-05-28 00:27:28 +0800

    feat: 初次提交

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,50 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  lang: "zh-CN",
+  base: "/my-blog/",
+  head: [["link", { rel: "icon", href: "/my-blog/favicon.ico" }]],
+  title: "打工仔的博客",
+  description: "一个技术记录的网站",
+  themeConfig: {
+    logo: "/bg.svg",
+    siteTitle: "打工仔的博客",
+    search: {
+      provider: "local",
+    },
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      // { text: '首页', link: '/' },
+      { text: "前端面试题", link: "/qdmst/JS-001.md" },
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: '前端面试题',
+    //     items: [
+    //       { text: '我的东方大厦防守打法撒旦法阿萨德阿萨德阿萨德速度山大', link: '/qdmst/JS-001.md' },
+    //       // { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+    sidebar: {
+      "/qdmst/": [
+        {
+          text: "前端面试题",
+          items: [
+            {
+              text: "我的东方大厦防守打法撒旦法阿萨德阿萨德阿萨德速度山大",
+              link: "/qdmst/JS-001.md",
+            },
+            { text: "Runtime API Examples", link: "/qdmst/JS-002.md" },
+          ],
+        },
+      ],
+    },
+
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ]
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "干五休二打工仔的博客"
+  text: "一个技术记录的网站"
+  tagline: 好好学习，天天向上
+  image:
+    src: /bg.svg
+    alt: blog
+  actions:
+    - theme: brand
+      text: 前端面试题
+      link: /qdmst/JS-001.md
+#    - theme: alt
+#      text: API Examples
+#      link: /api-examples
+
+#features:
+#  - title: Feature A
+#    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#  - title: Feature B
+#    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#  - title: Feature C
+#    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
